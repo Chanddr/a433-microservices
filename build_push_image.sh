9c8f3c896a0d04d94c337008d1b2bffa80cd6fbd
Chanddr <adiwijayachandra927@gmail.com>
2024-02-23 10:41:46 +0700

    Add stage

--- build_push_image.sh
+++ build_push_image.sh
@@ -7,10 +7,10 @@ docker build -t item-app:v1 .
 docker images
 
 #Mengubah nama image agar sesuai dengan format GitHub Packages
-docker tag item-app:v1 docker.pkg.github.com/Chanddr/repository/item-app:v1
+docker tag item-app:v1 ghcr.io/chanddr/docker-publish-dicoding/item-app:v1
 
 #Login ke GitHub Packages
 echo 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 | docker login docker.pkg.github.com -u Chanddr --password-stdin
 
 #Mengunggah image ke GitHub Packages (GitHub Container Registry)
-docker push docker.pkg.github.com/Chanddr/repository/item-app:v1
+docker push ghcr.io/chanddr/docker-publish-dicoding/item-app:v1
