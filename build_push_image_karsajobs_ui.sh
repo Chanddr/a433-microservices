04a5b4eb8779127b94cbcd5dcff620c83f3035e1
Chanddr <adiwijayachandra927@gmail.com>
2024-03-06 15:38:44 +0700

    Add File Bash

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Build Docker image untuk backend
-docker build -t chanddr/karsajobs-ui:latest .
+# Build Docker image untuk frontend
+docker build -t karsajobs-ui:latest .
+
+#Melihat daftar image di lokal
+docker images
+
+#Mengubah nama image agar sesuai dengan format GitHub Packages
+docker tag karsajobs-ui:latest ghcr.io/chanddr/docker-publish-karsajobs-ui/karsajobs-ui:latest
 
 #Login ke GitHub Packages
-echo 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 | docker login docker.pkg.github.com -u Chanddr --password-stdin
+echo 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 | docker login ghcr.io -u Chanddr --password-stdin
 
 #Mengunggah image ke GitHub Packages (GitHub Container Registry)
-docker push ghcr.io/chanddr/docker-publish-dicoding/kersajobs:latest
+docker push ghcr.io/chanddr/docker-publish-karsajobs-ui/karsajobs-ui:latest
