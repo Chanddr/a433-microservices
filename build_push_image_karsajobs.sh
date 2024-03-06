2dfd5d9ce24f691dbdd23a9d5b210b4ebc1f38d3
Chanddr <adiwijayachandra927@gmail.com>
2024-03-06 11:08:10 +0700

    Add Bash File

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build Docker image untuk backend
+docker build -t chanddr/karsajobs:latest .
+
+#Login ke GitHub Packages
+echo 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 | docker login docker.pkg.github.com -u Chanddr --password-stdin
+
+#Mengunggah image ke GitHub Packages (GitHub Container Registry)
+docker push ghcr.io/chanddr/docker-publish-dicoding/kersajobs:latest
